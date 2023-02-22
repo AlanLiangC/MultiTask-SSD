297301563a5ed29cc6d5ad535877c07fcc2cf75b
liangao <liangao@autowise.ai>
2023-02-22 02:21:13 +0000

    Add AL model

--- tools/test.py
+++ tools/test.py
@@ -23,7 +23,7 @@ warnings.filterwarnings('ignore', category=DeprecationWarning)
 
 def parse_config():
     parser = argparse.ArgumentParser(description='arg parser')
-    parser.add_argument('--cfg_file', type=str, default='./cfgs/nuscenes_models/MLT_SSD.yaml', help='specify the config for training')
+    parser.add_argument('--cfg_file', type=str, default='./cfgs/kitti_models/AL.yaml', help='specify the config for training')
 
     parser.add_argument('--batch_size', type=int, default=None, required=False, help='batch size for training')
     parser.add_argument('--workers', type=int, default=4, help='number of workers for dataloader')
