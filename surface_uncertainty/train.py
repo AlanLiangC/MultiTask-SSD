4104de19a1794d33ecaea740513572f87e4ebd80
liangao <liangao@autowise.ai>
2023-03-08 15:04:40 +0000

    Change V4

--- surface_uncertainty/train.py
+++ surface_uncertainty/train.py
@@ -119,7 +119,7 @@ def main():
     if args.fix_random_seed:
         common_utils.set_random_seed(666)
 
-    args.extra_tag = 'V4'
+    # args.extra_tag = 'V4'
 
     output_dir = Path(cfg.ROOT_DIR) / 'output' / cfg.EXP_GROUP_PATH / cfg.TAG / args.extra_tag
     ckpt_dir = output_dir / 'ckpt'
